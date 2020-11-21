689e77f1284b49b4fed97c3e7411fcec851a0205
Randall Nagy <randall.nagy@gmail.com>
2020-11-21 06:50:12 -0500

    The Classic Scriptures
    Forgot I had a version six - here it is!

---
+++ ClassicScriptures/sb.sh
@@ -0,0 +1,3 @@
+#/usr/jdk1.6.0_22/bin/java -jar /d_drive/BIN/SierraBible/*.jar -cp /usr/jdk1.6.0_22/lib/:/d_drive/BIN/SierraBible/lib/
+/usr/lib/jvm/java-6-sun-1.6.0.26/jre/bin/java -jar /d_drive/BIN/SierraBible/SierraBible5.jar -cp /d_drive/BIN/SierraBible/lib/ 
+
