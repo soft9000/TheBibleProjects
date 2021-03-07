80195b56f5d61267ce2d9c6489b7be36a61e4095
Randall Nagy <randall.nagy@gmail.com>
2021-03-07 09:17:34 -0500

    Modern Java =)
    Try this @ home ...

--- SierraBible/sb.sh
+++ SierraBible/sb.sh
@@ -1,3 +1,1 @@
-#/usr/jdk1.6.0_22/bin/java -jar /d_drive/BIN/SierraBible/*.jar -cp /usr/jdk1.6.0_22/lib/:/d_drive/BIN/SierraBible/lib/
-/usr/lib/jvm/java-6-sun-1.6.0.26/jre/bin/java -jar /d_drive/BIN/SierraBible/SierraBible5.jar -cp /d_drive/BIN/SierraBible/lib/ 
-
+java -jar SierraBible5.jar
