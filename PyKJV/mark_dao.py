44097e9b7842dc3eb84a215fe60d1b4b62cec4f6
= <=>
2021-04-04 09:14:06 -0400

    Bookmarks - For Mr. Bear 2 Test =)

--- PyKJV/mark_dao.py
+++ PyKJV/mark_dao.py
@@ -5,27 +5,49 @@ class BookMark:
     m_id = 0
     m_start = 0
     m_end = 0
+    m_user = 0
 
-    def __init__(self, start, end):
+    def __init__(self, start, end, zid=0, user=0):
+        self.m_id = zid
         self.m_start = start
         self.m_end = end
+        self.m_user = user
 
 
 class BookMarks:
     ''' Controller / API '''
     @staticmethod
     def GetBookmarks(user=None) -> list():
-        pass
+        ''' Get all of the BookMarks() for the default / user '''
+        if not user:
+            user = 0
+        cmd = f"SELECT ID, SierraStart, SierraEnd from UserSelects \
+            WHERE UserID = {user};"
+        conn = SierraDAO.Connect()
+        res = conn.execute(cmd)
+        results = list()
+        zrow = res.fetchone()
+        while zrow:
+            results.append(BookMark(zrow[1], zrow[2], zrow[0], user))
+            zrow = res.fetchone()
+        return results 
 
     @staticmethod
-    def Sync(bmk, user=None) -> bool:
+    def Sync(bmk) -> bool:
         if not isinstance(bmk, BookMark):
             return False
+        conn = SierraDAO.Connect()
+        res = None
         if bmk.m_id == 0:
             cmd = f"INSERT INTO UserSelects (SierraStart, SierraEnd, UserID) \
-                VALUES ({bmk.m_start}, {bmk.m_end}, {bmk.m_id});"
+                VALUES ({bmk.m_start}, {bmk.m_end}, {bmk.m_user});"
             conn = SierraDAO.Connect()
             res = conn.execute(cmd)
+        else:
+            cmd = f"UPDATE UserSelects SET (SierraStart, SierraEnd, UserID) \
+                VALUES ({bmk.m_start}, {bmk.m_end}, {bmk.m_user}) WHERE \
+                    ID = {bmk.m_id};"
+            res = conn.execute(cmd)
+        if res:
             conn.commit()
-            return True
         return False
